f8fb5adbacbd823df93f2930e167623ef42ecbf2
fs-dev-1 <53535675+fs-dev-1@users.noreply.github.com>
2019-09-30 15:23:55 -0400

    First Commit

---
+++ SampleUpdUserPromptPlugin/FileSelectionEditor.cs
@@ -0,0 +1,69 @@
+﻿using FabSoftUpd.Wizard;
+using System.Windows;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+namespace SampleUpdUserPromptPlugin
+{
+    public class FileSelectionEditor : ITypeEditor
+    {
+        private PropertyItem _propertyItem;
+        private FilePropertyGridControl _gridControl;
+
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        private static void AttachDebugger() // Attach debugger if none is already attached and it is compiled for DEBUG.
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+            else
+            {
+                System.Diagnostics.Debugger.Launch();
+            }
+        }
+
+        public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+        {
+            
+            AttachDebugger();
+            
+            _propertyItem = propertyItem;
+
+            _gridControl = new FilePropertyGridControl();
+
+            _gridControl.btnBrowse.Click += new RoutedEventHandler((object sender, RoutedEventArgs e) =>
+            {
+                string filePath = ((SampleFileUserPrompt)_propertyItem.Instance).FilePath;
+
+                System.Windows.Forms.OpenFileDialog opeFileDlg = new System.Windows.Forms.OpenFileDialog()
+                {
+                    FileName = filePath,
+                    Multiselect = false
+                };
+
+                if (opeFileDlg.ShowDialog(new Win32WindowWrapper(WpfWindowHelper.GetParentWindowHandle((UIElement)sender))) == System.Windows.Forms.DialogResult.OK)
+                {
+                    ((SampleFileUserPrompt)_propertyItem.Instance).FilePath = opeFileDlg.FileName;
+                }
+
+                RefreshTextField();
+            });
+
+            RefreshTextField();
+
+            return _gridControl;
+        }
+
+        public void RefreshTextField()
+        {
+            string newText = "<NULL>";
+            if (_propertyItem != null)
+            {
+                newText = ((SampleFileUserPrompt)_propertyItem.Instance).FilePath;
+            }
+            _gridControl.txtFilePath.Text = newText;
+        }
+    }
+}
---
+++ SampleUpdUserPromptPlugin/SampleUpdUserPromptPlugin.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+using FabSoftUpd.Wizard.Workflows_v1.UserPrompts;
+
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+using System.Collections.Generic;
+
+namespace SampleUpdUserPromptPlugin
+{
+    [Serializable]
+    public class SampleFileUserPrompt : UserPromptBase
+    {
+        public override string PromptTypeDescription { get { return "Sample Lookup File Prompt Plugin"; } }
+
+        [DisplayName("Max Line Length")]
+        public int MaxLength { get; set; } = int.MaxValue;
+
+        [DisplayName("Min Line Length")]
+        public int MinLength { get; set; } = 0;
+
+        [Browsable(false)] // To hide a property from being visible in the UI, use [Browsable(False)]
+        public int HiddenProperty { get; set; } = 0;
+
+        [DisplayName("Lookup File")]
+        [Xceed.Wpf.Toolkit.PropertyGrid.Attributes.PropertyOrder(1)] // To specify the order of properties, use [Xceed.Wpf.Toolkit.PropertyGrid.Attributes.PropertyOrder(#)]
+        [EditorAttribute(typeof(FileSelectionEditor), typeof(ITypeEditor))] // To customize the editor for this property in the Property Grid, use [EditorAttribute(typeof(<EDITOR>), typeof(ITypeEditor))]
+        public string FilePath { get; set; } = null;
+
+        public SampleFileUserPrompt()
+        {
+
+        }
+
+        public SampleFileUserPrompt(string name, string visible)
+        {
+            Name = name;
+            Visible = visible;
+        }
+
+
+        public override UserPromptBase Clone()
+        {
+            SampleFileUserPrompt newProperty = new SampleFileUserPrompt();
+            CopyTo(newProperty);
+            return newProperty;
+        }
+
+        public override bool Configure()
+        {
+            return true;
+        }
+
+        public override void CopyTo(UserPromptBase targetProperty)
+        {
+            if (typeof(SampleFileUserPrompt).IsAssignableFrom(targetProperty.GetType()))
+            {
+                CopyTo((SampleFileUserPrompt)targetProperty);
+            }
+        }
+
+        public void CopyTo(SampleFileUserPrompt targetProperty)
+        {
+            base.CopyTo(targetProperty);
+
+            targetProperty.MaxLength = this.MaxLength;
+            targetProperty.MinLength = this.MinLength;
+            targetProperty.HiddenProperty = this.HiddenProperty;
+            targetProperty.FilePath = this.FilePath;
+        }
+
+
+        [System.Diagnostics.Conditional("DEBUG")]
+        private static void AttachDebugger() // Attach debugger if none is already attached and it is compiled for DEBUG.
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+            else
+            {
+                System.Diagnostics.Debugger.Launch();
+            }
+        }
+
+        private PromptControls _PromptControls = null;
+        public override UIElement GetControl(out ValidateResult validator, string defaultValue)
+        {
+            AttachDebugger();
+            
+            validator = new ValidateResult((out string errorMessage) => {
+                errorMessage = null;
+                return true;
+            });
+
+            _PromptControls = new PromptControls();
+
+            _PromptControls.btnRefresh.Click += ((object sender, RoutedEventArgs e) =>
+            {
+                try
+                {
+                    _PromptControls.cmbLookup.ItemsSource = LoadFile();
+                }
+                catch (Exception ex)
+                {
+                    //TODO: Error
+                }
+            });
+
+            _PromptControls.cmbLookup.ItemsSource = LoadFile();
+            _PromptControls.cmbLookup.Text = defaultValue;
+
+            return _PromptControls;
+        }
+
+        private List<string> LoadFile()
+        {
+            List<string> fileData = new List<string>();
+            using (FileStream fs = new FileStream(this.FilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        fileData.Add(sr.ReadLine());
+                    }
+                }
+            }
+            return fileData;
+        }
+
+        public override bool Validate()
+        {
+            try
+            {
+                string resultValue = _PromptControls?.cmbLookup?.SelectedItem?.ToString();
+                if (resultValue != null && resultValue.Length >= MinLength && resultValue.Length <= MaxLength)
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO: Error
+            }
+            return false;
+        }
+
+        public override void CalculateResult()
+        {
+            UiResult = _PromptControls?.cmbLookup?.SelectedItem?.ToString();
+        }
+
+        public override void Reset()
+        {
+            //
+        }
+    }
+}
